85a101ae4d35417e3a13cb4fa107bae43a3d7098
Pavel Denysenko <49881075+PavelDeny@users.noreply.github.com>
2021-12-26 13:02:22 +0200

    Add files via upload

---
+++ js/responses.js
@@ -0,0 +1,10 @@
+function getBotResponse(input) {
+    // Simple responses
+    if (input == "Привет") {
+        return "Hello Man, How are you!";
+    } else if (input == "Пока") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!Try asking something else!Try asking something else!Try asking something else!";
+    }
+}
